7b75f464356511d01cec93072123014628b7ae35
Lander Brandt <landerbrandt@gmail.com>
2020-11-27 23:08:18 -0800

    Sort constant comparison finder results by number of unique results per function

--- constant_comparison_finder.py
+++ constant_comparison_finder.py
@@ -1,11 +1,11 @@
 #
 #
 def match_comparison_against_const(insn):
-	if insn.operation != HighLevelILOperation.HLIL_IF:
+	if insn.operation != LowLevelILOperation.LLIL_IF:
 		return False
-	if insn.condition.operation != HighLevelILOperation.HLIL_CMP_NE and insn.condition.operation != HighLevelILOperation.HLIL_CMP_E:
+	if insn.condition.operation != LowLevelILOperation.LLIL_CMP_NE and insn.condition.operation != LowLevelILOperation.LLIL_CMP_E:
 		return False
-	if insn.condition.right.operation != HighLevelILOperation.HLIL_CONST:
+	if insn.condition.right.operation != LowLevelILOperation.LLIL_CONST:
 		return False
 	return True
 
@@ -17,7 +17,7 @@ while functions:
 	if function is None:
 		continue
 
-	for block in function.hlil:
+	for block in function.llil:
 		for insn in block:
 			if match_comparison_against_const(insn):
 				constant = insn.condition.right.constant
@@ -27,9 +27,17 @@ while functions:
 					found_consts[function].append((insn.address, constant))
 
 
+sorted_results = []
 for function, consts in found_consts.items():
 	found_matches_unique = list(set(map(lambda const_data: hex(const_data[1]), consts)))
+	num_matches = len(found_matches_unique)
 	found_matches_unique.sort()
+	sorted_results.append((function, consts, num_matches, found_matches_unique))
+
+sorted_results = sorted(sorted_results, key=lambda res: res[2])
+
+for res in sorted_results:
+	function, consts, count, found_matches_unique = res
 	joined_matches = ", ".join(found_matches_unique)
 	print("{}: {}".format(function.name, joined_matches))
 	for const_data in consts:
